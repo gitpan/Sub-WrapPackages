ca13f71ffe969511c00819888e4ae7a1b7fc7740
David Cantrell <david@cantrell.org.uk>
2003-11-15 19:27:13 +0000

    Import of DCANTRELL/Sub-WrapPackages-1.0 from CPAN.
    gitpan-cpan-distribution: Sub-WrapPackages
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         DCANTRELL/Sub-WrapPackages-1.0.tar.gz
    gitpan-cpan-author:       DCANTRELL
    gitpan-cpan-maturity:     released

---
+++ lib/Sub/WrapPackages.pm
@@ -0,0 +1,137 @@
+use strict;
+use warnings;
+
+package Sub::WrapPackages;
+
+use vars qw($VERSION);
+
+$VERSION = '1.0';
+
+=head1 NAME
+
+Sub::WrapPackages - add pre- and post-execution wrappers around all the
+subroutines in packages or around individual subs
+
+=head1 SYNOPSIS
+
+    use Sub::WrapPackages (
+        packages => [qw(Foo Bar)],        # wrap all Foo::* and Bar::*
+        subs     => [qw(Baz::a, Baz::b)], # wrap these two subs as well
+	pre      => sub {
+	    print "called $_[0] with params ".
+	      join(', ', @_[1..$#_])."\n";
+	},
+	post     => sub {
+	    print "$_[0] returned $_[1]\n";
+	},
+
+=head1 DESCRIPTION
+
+This is mostly a wrapper around Damian Conway's Hook::LexWrap module.
+Please go and read the docs for that module now.  The differences are:
+
+=over 4
+
+=item no exporting
+
+We don't export a wrap() function, instead preferring to do all the magic
+when you C<use> this module.  We just wrap named subroutines, no references.
+I didn't need that functionality so although it's probably available if
+you look at the source I haven't tested it.  Patches welcome!
+
+=item the subs and packages arrayrefs
+
+In the synopsis above, you will see two named parameters, C<subs> and
+C<packages>.  Any subroutine mentioned in C<subs> will be wrapped.  Any
+packages mentioned in C<packages> will have all their subroutines wrapped.
+We divine which subs to wrap in a package with the C<_get_syms> method from
+Richard Clamp's Pod::Coverage module.  Yes, it's documented as being
+unstable.  Caveat User.
+
+=item parameters passed to your subs
+
+I threw Damian's ideas out of the window.  Instead, your pre-wrapper will
+be passed the wrapped subroutine's name, and all the parameters to be passed
+to it.  Who knows what will happen if you modify those params, I don't
+need that so haven't tested it.  Patches welcome!
+
+The post-wrapper will be passed the wrapped subroutine's name, and a single
+parameter for the return value(s) as in Damian's module.  Figuring out the
+difference between returning an array and returning a reference to an array
+is left as an exercise for the interested reader.
+
+=back
+
+=head1 BUGS
+
+Wrapped subroutines may cause perl 5.6.1, and maybe other versions, to
+segfault when called in void context.  I believe this is a bug in
+Hook::LexWrap.
+
+I say "patches welcome" a lot.
+
+AUTOLOAD and DESTROY are not treated as being special.
+
+=head1 FEEDBACK
+
+I like to know who's using my code.  All comments, including constructive
+criticism, are welcome.  Please email me.
+
+=head1 AUTHOR
+
+David Cantrell E<lt>F<david@cantrell.org.uk>E<gt>
+
+Thanks also to Adam Trickett who thought this was a jolly good idea.
+
+=head1 COPYRIGHT
+
+Copyright 2003 David Cantrell
+
+This module is free-as-in-speech software, and may be used, distributed,
+and modified under the same terms as Perl itself.
+
+=cut
+
+use Hook::LexWrap;
+use Pod::Coverage;  # we use _get_syms out of here
+
+sub import {
+    my $i_am_weasel = shift;
+    wrapsubs(@_) if(@_);
+}
+
+sub subs_in_packages {
+    my @targets = @_;
+
+    my @subs;
+    foreach my $package (@targets) {
+        push @subs, map {
+            $package.'::'.$_
+        } Pod::Coverage->new(
+            package => $package, private => []
+        )->_get_syms($package);
+    }
+    return @subs;
+}
+
+sub wrapsubs {
+    my %params = @_;
+
+    if(exists($params{packages}) && ref($params{packages}) =~ /^ARRAY/) {
+        push @{$params{subs}}, subs_in_packages(@{$params{packages}});
+    } elsif(exists($params{packages})) {
+        die("Bad param 'packages'");
+    }
+
+    return undef if(!$params{pre} && !$params{post});
+
+    foreach my $sub (@{$params{subs}}) {
+        Hook::LexWrap::wrap($sub, (($params{pre}) ?
+            (pre =>  sub { &{$params{pre}}($sub, @_[0..$#_-1]) }) : ()
+        ),(($params{post}) ?
+            (post => sub { &{$params{post}}($sub, $_[-1]) }) : ()
+        ));
+    }
+}
+
+1;
---
+++ t/01_wrap_as_packages.t
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+my $r;
+
+use strict;
+
+BEGIN { $| = 1; print "1..2\n"; }
+
+use lib 't/lib'; use a;
+use Data::Dumper;
+use Sub::WrapPackages (
+    packages => [qw(a)],
+    pre => sub {
+        $r .= join(", ", @_);
+    },
+    post => sub {
+        $r .= ref($_[1]) =~ /^ARRAY/ ? @{$_[1]} : $_[1];
+    }
+);
+
+my $test = 0;
+
+$r .= a::a_scalar(1..3);
+
+print 'not ' unless($r eq 'a::a_scalar, 1, 2, 3in sub a_scalarin sub a_scalar');
+print 'ok '.(++$test)." returning scalar in scalar context\n";
+
+$r = '';
+my @r = a::a_list(4,6,8);
+
+print 'not ' unless(join('', @r) eq 'insuba_list' && $r eq 'a::a_list, 4, 6, 83');
+print 'ok '.(++$test)." returning array in array context\n";
+
